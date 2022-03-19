b3a363669bf6f81277af62d1dc5b407008e1352a
ramiro1987 <86367460+ramiro1987@users.noreply.github.com>
2022-03-19 18:20:48 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CNavbar from './Components/Navbar/CNavbar';
+function App() {
+  return (
+    
+    <div className="App">
+   <CNavbar/>
+    </div>
+  );
+}
+
+export default App;
